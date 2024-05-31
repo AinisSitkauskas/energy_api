8322eaf1322aaeeb116c337890dfafcb8ce75368
Ainis <ainis795@gmail.com>
2024-05-31 17:13:28 +0300

    Added command for sending feedbacks

---
+++ src/Command/SendUserFeedbackCommand.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Command;
+
+use App\Repository\UsersRepository;
+use App\Service\Handler\Feedback\SendUserFeedbackHandler;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class SendUserFeedbackCommand extends Command
+{
+    public function __construct(
+        private readonly SendUserFeedbackHandler $sendUserFeedbackHandler,
+        private readonly UsersRepository $usersRepository,
+    ) {
+        parent::__construct();
+    }
+
+    #[AsCommand(name: 'app:send-user-feedback')]
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $users = $this->usersRepository->findBy(['hasFeedbackReport' => true]);
+
+        foreach ($users as $user) {
+            $this->sendUserFeedbackHandler->handle($user);
+        }
+
+        return Command::SUCCESS;
+    }
+}
